18ff1b5b82cdbc938dff129650b04dd9df5bbd49
Pavel <t1m0kh0v14@gmail.com>
2022-08-10 00:32:04 +0300

    Доработка валидации

--- lesson03/calc.go
+++ lesson03/calc.go
@@ -2,63 +2,76 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
 
 func main() {
 	var (
-		a, b   float64
-		op     string
-		repeat bool = true
+		op1, op2, operation     string
 	)
 
+	repeat := true
 	for repeat {
 		fmt.Print("Введите первое число: ")
-		fmt.Scan(&a)
+		fmt.Scan(&op1)
+		fA, err := strconv.ParseFloat(op1, 64)
+		if err != nil {
+			fmt.Println("Введено не число,  повторите попытку")
+			continue
+		}
 
 		fmt.Print("Введите второе число: ")
-		fmt.Scan(&b)
+		fmt.Scan(&op2)
+		fB, err := strconv.ParseFloat(op2, 64)
+		if err != nil {
+			fmt.Println("Введено не число,  повторите попытку")
+			continue
+		}
 
 		fmt.Print("Выберите оператор из списка(+-*/^!): ")
-		//\n+ сложение\n- вычитание\n* умножение\n/ деление\n^ возведение в степень\n! факторил):\n ")
-		fmt.Scan(&op)
+		fmt.Scan(&operation)
 
-		switch op {
+		switch operation {
 		case "+":
-			fmt.Printf("%.0f + %.0f = %.0f\n", a, b, a+b)
+			fmt.Printf("%.2f + %.2f = %.2f\n", fA, fB, fA+fB)
 		case "-":
-			fmt.Printf("%.0f - %.0f = %.0f\n", a, b, a-b)
+			fmt.Printf("%.2f  */- %.2f = %.2f\n", fA, fB, fA-fB)
 		case "*":
-			fmt.Printf("%.0f * %.0f = %.0f\n", a, b, a*b)
+			fmt.Printf("%.2f * %.2f = %.2f\n", fA, fB, fA*fB)
 		case "/":
-			if b == 0 {
+			if fB == 0 {
 				fmt.Println("Деление на 0 запрещено")
 				break
 			} else {
-				fmt.Printf("%.0f / %.0f = %.2f\n", a, b, a/b)
+				fmt.Printf("%.2f / %.2f = %.2f\n", fA, fB, fA/fB)
 			}
 		case "^":
-			pow := 1.0
-			for i := 1.0; i <= b; i++ {
-				pow *= a
-			}
-			fmt.Printf("%.0f ^ %.0f = %.0f\n", a, b, pow)
+			fmt.Printf("%.2f ^ %.2f = %.2f\n", fA, fB, math.Pow(fA, fB))
 		case "!":
-			fa := 1.0
-			fb := 1.0
-			if a < 0 || b < 0 {
-
-			} else {
-				for i := 1.0; i <= a; i++ {
-					fa *= i
-				}
-				for i := 1.0; i <= b; i++ {
-					fb *= i
-				}
+			iA, err := strconv.ParseUint(op1, 10, 64)
+			iB, err := strconv.ParseUint(op2, 10, 64)
+			if err != nil {
+				fmt.Println("Факториал определен только для целых неотрицательных чисел")
+				continue
 			}
-			fmt.Printf("!%.0f = %.0f и !%.0f = %.0f\n", a, fa, b, fb)
+			if fA < 0 || fB < 0 {
+				fmt.Println("Факториал определен только для целых неотрицательных чисел")
+				continue
+			} else {
+			}
+			fmt.Printf("!%d = %d и !%d = %d\n", iA, fac(iA), iB, fac(iB))
 		default:
 			repeat = false
 			fmt.Println("Операция не найдена.")
 		}
+	}
+}
+
+func  fac(n uint64) uint64 {
+	if n == 0 {
+		return 1
+	} else {
+		return n * fac(n - 1)
 	}
 }
