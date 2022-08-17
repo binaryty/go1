4fc38f576af32d7704609820358932aeec4212c5
Pavel <t1m0kh0v14@gmail.com>
2022-08-17 15:19:43 +0300

    Add cache optimization

--- lesson05/fibo.go
+++ lesson05/fibo.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+var (
+	cache = make(map[int]uint64)
+)
+
 func simpleFibo(n int) uint64 {
 	if n < 2 {
 		return uint64(n)
@@ -9,9 +13,25 @@ func simpleFibo(n int) uint64 {
 	return simpleFibo(n-1) + simpleFibo(n-2)
 }
 
+func cachedFibo(n int) uint64 {
+	if n < 2 {
+		return uint64(n)
+	}
+
+	if value, ok := cache[n]; ok {
+		return value
+	}
+
+	fiboResilt := cachedFibo(n-1) + cachedFibo(n-2)
+	cache[n] = fiboResilt
+
+	return fiboResilt
+}
+
 func main() {
-	for i := 0; i < 15; i++ {
-		fmt.Printf("%d ", simpleFibo(i))
+	for i := 0; i < 25; i++ {
+		// fmt.Printf("%d ", simpleFibo(i))
+		fmt.Printf("%d ", cachedFibo(i))
 	}
 	fmt.Print("\n")
 }
