db2be1d22432720c001c4554547b63b8a7a2cf77
Pavel Timokhovich <t1m0kh0v14@gmail.com>
2022-08-26 22:01:17 +0300

    Fix format

--- lesson07/interface.go
+++ lesson07/interface.go
@@ -15,9 +15,9 @@ func (c *ReaderWriter) Read(b []byte) (int, error) {
 	if c.index >= len(c.buffer) {
 		return 0, io.EOF
 	}
-  count := copy(b, c.buffer[c.index:])
-  c.index += count
-  return count, nil
+	count := copy(b, c.buffer[c.index:])
+	c.index += count
+	return count, nil
 }
 
 func (c *ReaderWriter) Write(b []byte) (int, error) {
@@ -26,7 +26,7 @@ func (c *ReaderWriter) Write(b []byte) (int, error) {
 }
 
 func main() {
-	c := &ReaderWriter{}
+	var c = &ReaderWriter{}
 
 	l, err := io.WriteString(c, "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.")
 	if err != nil {
