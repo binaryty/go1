abe837c5e485f76d66e1fccdd90ab5925e8fb27b
Pavel Timokhovich <t1m0kh0v14@gmail.com>
2022-08-27 00:46:24 +0300

    Add clear buffer func

--- lesson07/interface.go
+++ lesson07/interface.go
@@ -11,32 +11,42 @@ type ReaderWriter struct {
 	index  int
 }
 
-func (c *ReaderWriter) Read(b []byte) (int, error) {
-	if c.index >= len(c.buffer) {
+func (rw *ReaderWriter) Clear(b []byte) {
+	if len(rw.buffer) > 0 {
+		rw.buffer = rw.buffer[:0]
+	}
+	if len(b) > 0 {
+		b = b[:0]
+	}
+}
+
+func (rw *ReaderWriter) Read(b []byte) (int, error) {
+	if rw.index >= len(rw.buffer) {
+		rw.Clear(b)
 		return 0, io.EOF
 	}
-	count := copy(b, c.buffer[c.index:])
-	c.index += count
+	count := copy(b, rw.buffer[rw.index:])
+	rw.index += count
 	return count, nil
 }
 
-func (c *ReaderWriter) Write(b []byte) (int, error) {
-	c.buffer = append(c.buffer, b...)
+func (rw *ReaderWriter) Write(b []byte) (int, error) {
+	rw.buffer = append(rw.buffer, b...)
 	return len(b), nil
 }
 
 func main() {
-	var c = &ReaderWriter{}
+	var rw = &ReaderWriter{}
 
-	l, err := io.WriteString(c, "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.")
+	l, err := io.WriteString(rw, "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.")
 	if err != nil {
 		log.Fatalf("Ошибка записи: %v", err)
 	}
-	fmt.Printf("< Запись в буфер: %d байт >\n%s\n\n", l, c.buffer)
+	fmt.Printf("< Запись в буфер: %d байт >\n%s\n\n", l, rw.buffer)
 
-	buf, err := io.ReadAll(c)
+	buf, err := io.ReadAll(rw)
 	if err != nil {
 		log.Fatalf("Ошибка чтения: %v", err)
 	}
-	fmt.Printf("< Чтение из буфера: %d байт >\n%s\n", len(c.buffer), string(buf))
+	fmt.Printf("< Чтение из буфера: %d байт >\n%s\n", rw.index, string(buf))
 }
